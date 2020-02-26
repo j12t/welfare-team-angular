c2cc19f550aecee040bdc0ee0aeff45752fe9e3d
J12T <thibault.jerome@gmail.com>
2020-02-26 23:30:15 +0100

    adding list of moods

--- src/app/mood-list/mood-list.component.ts
+++ src/app/mood-list/mood-list.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Mood } from '../model/mood';
+import { Page } from '../model/page';
 import { MoodService } from '../service/mood-service.service';
- 
+
 @Component({
   selector: 'app-mood-list',
   templateUrl: './mood-list.component.html',
   styleUrls: ['./mood-list.component.less']
 })
 export class MoodListComponent implements OnInit {
- 
+
+//  moods: Page<Mood>;
   moods: Mood[];
- 
+
   constructor(private moodService: MoodService) {
   }
- 
+
   ngOnInit() {
     this.moodService.findAll().subscribe(data => {
-      this.moods = data;
+      this.moods = data._embedded.items;
     });
   }
 }
--- src/app/service/mood-service.service.ts
+++ src/app/service/mood-service.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Mood } from '../model/mood';
+import { Page } from '../model/page';
 import { Observable } from 'rxjs/Observable';
- 
+import { catchError, map, tap} from 'rxjs/operators';
+
 @Injectable()
 export class MoodService {
- 
+
   private moodsUrl: string;
- 
+
   constructor(private http: HttpClient) {
     this.moodsUrl = 'http://localhost:8081/welfare-team-api/v1/moods';
   }
- 
-  public findAll(): Observable<Mood[]> {
-    return this.http.get<Mood[]>(this.moodsUrl);
+
+  public findAll(): Observable<Page<Mood>> {
+    return this.http.get<Page<Mood>>(this.moodsUrl);
   }
- 
+
   public save(mood: Mood) {
     return this.http.post<Mood>(this.moodsUrl, mood);
   }
