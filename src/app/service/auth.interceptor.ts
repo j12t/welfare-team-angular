87bf99c2dac9bc50cc80e4d213ad4c9326e90185
J12T <thibault.jerome@gmail.com>
2020-03-10 23:15:51 +0100

    finaliza with header Authorization Bearer

--- src/app/service/auth.interceptor.ts
+++ src/app/service/auth.interceptor.ts
@@ -10,11 +10,14 @@ export class AuthInterceptor implements HttpInterceptor {
   
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
-    request = request.clone({
-      setHeaders: {
-        "X-Authorization": `Bearer ${this.auth.getToken()}`
-      }
-    });
+    console.log("**********  token : " + this.auth.getToken())
+    if (this.auth.getToken()) {
+      request = request.clone({
+        setHeaders: {
+          "Authorization": `Bearer ${this.auth.getToken()}`
+        }
+      });
+    }
     return next.handle(request);
   }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -13,19 +13,15 @@ export class AuthService {
 
   login(credentials, callback) {
   
-    console.log("111111111");
-
     this.http.post(this.tokenUrl, credentials).subscribe(response => this.setSession(response));
     return callback && callback();
   }
           
   private setSession(authResult) {
-    console.log("22222222 stockage ");
     const expiresAt = Date.now() + authResult.expiresIn * 1000;
 
     localStorage.setItem('token', authResult.token);
     localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
-    console.log(this.getToken());
   }          
 
   getToken() {
